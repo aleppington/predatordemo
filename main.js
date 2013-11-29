5abe70e74c7dda1d56eaa5bf15f71d6d44c3328b
Adam Cunliffe <adam.cunliffe@quantiv.com>
2013-11-29 11:47:57 +0000

    added birth and death rates to model

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 function pp_start() {
 
 
-    var prey = {name : 'Prey', size: 75, x: 400};
-    var predator = {name:'Predator', size:50, x:200};
+    var prey = 
+    {
+        name : 'Prey', 
+        size: 75, 
+        x: 400,
+        birthRate: 0.16,
+        deathRate: 1
+    };
+
+    var predator = 
+    {
+        name:'Predator', 
+        size:50, 
+        x:200,
+        birthRate: 1,
+        deathRate: 0.12
+    };
 
     var stocks = [predator, prey];
 
@@ -11,14 +26,14 @@ function pp_start() {
     .attr('height', 600);
 
    function doUpdate() {
-     console.log('doUpdate called');
-     stocks[0].size += 10;
-     stocks[1].size -= 10;
+
+     predator.size += (predator.size * predator.deathRate);
+     prey.size -= (prey.size * prey.birthRate);
+     
      update(svg, stocks);
    };
   
    setInterval(doUpdate, 100);
-  //doUpdate();
 };
 
 function update(svg, stocks){
