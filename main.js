bc466fa39b8660f4c01838ef53f102cb2eab2f8d
Adam Cunliffe <adam.cunliffe@quantiv.com>
2013-11-29 10:54:00 +0000

    bound model to d3 for drawing

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 function pp_start() {
 
-    var prey = {name : 'Prey', size: 75};
-    var predator = {name:'Predator', size:50};
+
+    var prey = {name : 'Prey', size: 75, x: 400};
+    var predator = {name:'Predator', size:50, x:200};
+
+    var stocks = [predator, prey];
 
   var svg = d3.select('#container').append('svg:svg')
     .attr('width', 600)
     .attr('height', 600);
-    
-    svg.append('svg:circle')
-        .attr('cx', 200)
-        .attr('cy',200)
-        .attr('r', predator.size);
-     
-    svg.append('svg:circle')
-        .attr('cx', 400)
-        .attr('cy',200)
-        .attr('r', prey.size);
+
+    var cells = svg.selectAll("circle")
+      .data(stocks);
+
+    cells.enter()
+      .append("circle")
+        .attr('cx',  function(d) { return d.x;})
+        .attr('cy', 200)
+        .attr('r', function(d) { return d.size;});
 
     svg.append('text')
         .text(predator.name)
