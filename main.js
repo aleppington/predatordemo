a628d4cefbd392c29e276307d704cdd3d7e741c5
Adam Cunliffe <adam.cunliffe@quantiv.com>
2013-11-29 15:18:29 +0000

    renaming stuff

--- main.js
+++ main.js
@@ -1,83 +1,85 @@
 function pp_start() {
 
 
-    var prey = 
+    var prey =
     {
-        name : 'Prey', 
-        size: 164, 
+        name: 'Prey',
+        size: 164,
         x: 400,
-        birthRate: 0.16,
-        deathRate: 1
+        inflow: 0.16,
+        outflow: 1
     };
 
-    var predator = 
+    var predator =
     {
-        name:'Predator', 
-        size:112, 
-        x:200,
-        birthRate: 1,
-        deathRate: 0.12
+        name: 'Predator',
+        size: 112,
+        x: 200,
+        inflow: 1,
+        outflow: 0.12
     };
 
     var stocks = [predator, prey];
 
-    var svg = initialiseView('#container');
+    var view = initialiseView('#container');
 
-   function doUpdate(iteration, interval, calculator) {
-
-    if (iteration <= 0) return;
-
-        calculator();
-
-        updateView(svg, stocks);
-        setTimeout(function() {doUpdate(iteration-1, interval, calculator);}, interval);
-   };
-  
-   doUpdate(100, 100, function() {performEquations(predator, prey);});
+    iterate(100, 100, function () { return performEquations(stocks); }, view);
 
 };
 
-function performEquations(predator, prey) {
-    prey.deathRate = predator.size * 0.0008;
-    predator.birthRate = prey.size * 0.001;
+function iterate(iterations, interval, calculator, view) {
+    if (iterations <= 0) return;
+    var stocks = calculator();
+    updateView(view, stocks);
+    setTimeout(function () { iterate(iterations - 1, interval, calculator, view); }, interval);
+};
 
-    predator.size = getAdjustedStock(predator.size, predator.birthRate, predator.deathRate);
-    prey.size = getAdjustedStock(prey.size, prey.birthRate, prey.deathRate);
+function performEquations(stocks) {
+    var predator = stocks[0];
+    var prey = stocks[1];
+ 
+    prey.outflow = predator.size * 0.0008;
+    adjustStock(prey, prey.inflow, prey.outflow);
+ 
+    predator.inflow = prey.size * 0.001;
+    adjustStock(predator, predator.inflow, predator.outflow);
+
+    return stocks;
 }
 
 function initialiseView(divId) {
-      var svg = d3.select(divId).append('svg:svg')
+    var svg = d3.select(divId).append('svg:svg')
     .attr('width', 600)
     .attr('height', 600);
     return svg;
 }
 
-function getAdjustedStock(size, inflowRate, outflowRate){
-    var reduction = size * outflowRate;
-    var increase = size * inflowRate;
-    var newSize = size + increase - reduction;
+function adjustStock(stock, inflowRate, outflowRate) {
+    var reduction = stock.size * outflowRate;
+    var increase = stock.size * inflowRate;
+    var newSize = stock.size + increase - reduction;
 
-    return Math.max(newSize,0);
+    stock.size = Math.max(newSize, 0);
 }
 
-function updateView(svg, stocks){
-     var stockDisplays = svg.selectAll("g")
+function updateView(svg, stocks) {
+    var stockDisplays = svg.selectAll("g")
       .data(stocks);
 
     var g = stockDisplays.enter().append('svg:g');
     g.append("rect")
-        .attr('x',  function(d) { return d.x;})
-        .attr('width',100);
+        .attr('x', function (d) { return d.x; })
+        .attr('width', 100);
 
     g.append('text')
-        .attr('x', function (d) {return d.x;})
+        .attr('x', function (d) { return d.x; })
         .attr('y', 580)
-        .attr('stroke','white');
+        .attr('stroke', 'white');
 
     stockDisplays.select("rect")
-            .attr('height', function(d)  { return d.size;})
-            .attr('y', function(d) {return 560 - d.size;});
+            .attr('height', function (d) { return d.size; })
+            .attr('y', function (d) { return 560 - d.size; });
 
     stockDisplays.select("text")
-             .text(function (d) { return d.name + ' (' + Math.round(d.size * 100)/100 + ')';});
+             .text(function (d) { return d.name + ' (' + Math.round(d.size * 100) / 100 + ')'; });
 }
