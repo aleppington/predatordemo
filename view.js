a451a9069d2d7204de013e7561ae380759cb8ec1
aleppington <andy.leppington@quantiv.com>
2013-12-30 11:18:00 +0000

    Slider implementation

--- view.js
+++ view.js
@@ -2,11 +2,11 @@ var View = function(parentElementId)
 {
     var parent = d3.select(parentElementId);
     var _dataView = new DataView(parent, 'dataView', 600, 600);
-    var _timeView = new TimeView(parent,'timeView', 600, 100);
+    var _timeView = new TimeView(parent,'timeView', 600, 50);
 
     this.update = function(modelData)
     {
-        _timeView.initialise(0, modelData.length, 20, 580, 0, 50);
+        _timeView.initialise(0, modelData.length, 0, 580, 0, 20);
         iterate(100, modelData, 0);
     };
 
@@ -54,22 +54,38 @@ var DataView = function(parent,controlId, width, height)
 
 var TimeView = function(parent,controlId, width, height)
 {
-    var _view = parent.append('svg:svg')
-        .attr('id', controlId)
+    var _view = parent.append('div')
+        .attr('id', controlId);
+        
+    var _timeLine = _view.append('svg:svg')
+        .attr('id', controlId + "_timeline")
         .attr('width', width)
         .attr('height', height);
-
+        
+    var _slider =  _view.append('input')
+            .attr('id', controlId + "_slider")
+            .attr('type', 'range')
+            
+        
     this.initialise = function(min, max, rangeMin, rangeMax, xPos, yPos)
     {
         var x = d3.scale.linear().domain([min, max]).range([rangeMin, rangeMax]);
+        
+        _slider.attr('min', min.toString())
+            .attr('max', max.toString())
+            .attr('style', 'width:' +  + 'px');
+            
         var axis = d3.svg.axis()
             .scale(x);
-        var axisElement = _view.append('g')
-            .attr('transform','translate(' + xPos + ',' + yPos + ')')
+            
+        var axisElement = _timeLine.append('g')
             .data(d3.range(rangeMin, rangeMax))
             .call(axis);
+            
         return axisElement;
     };
+    
+    
 };
 
 
