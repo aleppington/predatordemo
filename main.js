1216ac08e523ddd872adaad894be138dd640f7ca
aleppington <andy.leppington@quantiv.com>
2013-12-31 12:33:56 +0000

    Timeline binding to model

--- main.js
+++ main.js
@@ -1,57 +1,83 @@
-function start() {
-
-    var timeSpan = 500;
-
-    var view = new View("#container");
-    var model = createModel();
-    var data = runModel(timeSpan, function () { return step(model); });
-
-    view.update(data);
-
+var App = function()
+{
+    var _currentTime;
+    var _timeSpanMax = 500;
+    var _view = new View("#container");
+    var _model= createModel();
+    var _data;
+    
+    this.start = function() {
+        _data = runModel(_timeSpanMax, function () { return step(_model); });
+        _currentTime = 0;
+        _view.initialise(_currentTime, _timeSpanMax);
+        _view.addTimeChangedListener(update);
+    };
+    
+    var update = function(timeSpan) {
+        _view.update(_data[timeSpan]);
+    };
+    
+    var runModel = function(timeLength, calculator){
+        var data = new Array();
+        for (var j=0; j< timeLength; j++)
+        {
+            var model = calculator();
+            var timeSlotData = {
+                time: j,
+                stocks : new Array()
+            };
+            for (var i = 0; i < model.stocks.length; i++)
+            {
+                var currentStock = model.stocks[i];
+                timeSlotData.stocks.push(
+                    {
+                        name: currentStock.name,
+                        value: currentStock.size,
+                        x: currentStock.x
+                    });
+            }
+            data.push(timeSlotData);
+        }
+        return data;
+    };
+    
+    var step = function step(model) {
+        for (var i = model.controls.length - 1; i >= 0; i--) {
+            model.controls[i].execute();
+        };
+    
+        for (var i = model.stocks.length - 1; i >= 0; i--) {
+            adjustStock(model.stocks[i]);
+        };
+        return model;
+    };
+    
+    var adjustStock = function(stock) {
+        var reduction = stock.size * stock.outflow.rate;
+        var increase = stock.size * stock.inflow.rate;
+        var newSize = stock.size + increase - reduction;
+        stock.size = Math.max(newSize, 0);
+    };
 };
 
-function runModel(timeLength, calculator){
-
-    var data = new Array();
-
-    for (var j=0; j< timeLength; j++)
-    {
-        var model = calculator();
-        var timeSlotData = {
-            time: j,
-            stocks : new Array()
-        };
-        for (var i = 0; i < model.stocks.length; i++)
-        {
-            var currentStock = model.stocks[i];
-            timeSlotData.stocks.push(
-                {
-                    name: currentStock.name,
-                    value: currentStock.size,
-                    x: currentStock.x
-                });
-        }
-        data.push(timeSlotData);
-    }
-    return data;
-}
 
 
-function step(model) {
+//function onTimeChange()
+//{
+//    view.update(data[dataIndex]);
+//}
 
-    for (var i = model.controls.length - 1; i >= 0; i--) {
-        model.controls[i].execute();
-    };
+//function iterate(interval, data, view, dataIndex) {
+//        if (dataIndex >= data.length) return;
+//        view.update(data[dataIndex]);
+//        _timeView.update(dataIndex);
+//        dataIndex++;
+//        setTimeout(function () { iterate(interval, data, view, dataIndex); }, interval);
+//    };
 
-    for (var i = model.stocks.length - 1; i >= 0; i--) {
-        adjustStock(model.stocks[i]);
-    };
-    return model;
-}
 
-function adjustStock(stock) {
-    var reduction = stock.size * stock.outflow.rate;
-    var increase = stock.size * stock.inflow.rate;
-    var newSize = stock.size + increase - reduction;
-    stock.size = Math.max(newSize, 0);
-}
+
+
+
+
+
--- view.js
+++ view.js
@@ -2,21 +2,26 @@ var View = function(parentElementId)
 {
     var parent = d3.select(parentElementId);
     var _dataView = new DataView(parent, 'dataView', 600, 600);
-    var _timeView = new TimeView(parent,'timeView', 600, 40, 25);
-
-    this.update = function(modelData)
+    var _timeChangedListeners = new Array();
+    var _timeView = new TimeView(parent, _timeChangedListeners, 'timeView', 600, 40, 25, 0);
+    
+    this.initialise = function(min, max)
     {
-        _timeView.initialise(0, modelData.length);
-        iterate(100, modelData, 0);
+        _timeView.initialise(min, max);
     };
-
-    var iterate = function(interval, data, dataIndex) {
-        if (dataIndex >= data.length) return;
-        _dataView.update(data[dataIndex]);
-        //_timeView.update(dataIndex);
-        dataIndex++;
-        setTimeout(function () { iterate(interval, data, dataIndex); }, interval);
+    
+    this.update = function(data)
+    {
+        _dataView.update(data);
     };
+    
+    this.addTimeChangedListener = function(timeChangedListener)
+    {
+        _timeChangedListeners.push(timeChangedListener);
+    };
+    
+    
+ 
 };
 
 var DataView = function(parent,controlId, width, height)
@@ -52,11 +57,15 @@ var DataView = function(parent,controlId, width, height)
 };
 
 
-var TimeView = function(parent,controlId, width, height, padding)
+var TimeView = function(parent, timeChangedListeners,controlId, width, height, padding)
 {
+    var _min = 0;
+    var _max = 0;
+
     var _width = width;
     var _height = height;
     var _padding = padding;
+    var _onChangeListeners = timeChangedListeners;
     
     var _view = parent.append('div')
         .attr('id', controlId)
@@ -78,10 +87,20 @@ var TimeView = function(parent,controlId, width, height, padding)
         .attr('id', controlId + "_sliderinput")
         .attr('type', 'range')
         .attr('style', 'width:' + (_width - 120)  + 'px;');
+         
+    _sliderInput.on('mouseup', function() {
+        for (var i=0;i < _onChangeListeners.length; i++)
+        {
+            _onChangeListeners[i](parseInt(_sliderInput.property('value')));
+        }
+    });
             
-        
+    
     this.initialise = function(min, max)
     {
+        _min = min;
+        _max = max;
+        
         var rangeMin = 0;
         var rangeMax = (_width - 120);
         
@@ -89,6 +108,7 @@ var TimeView = function(parent,controlId, width, height, padding)
         
         _sliderInput.attr('min', min.toString())
             .attr('max', max.toString())
+            .attr('value', min.toString());
             
         var axis = d3.svg.axis()
             .scale(x);
@@ -113,6 +133,9 @@ var TimeView = function(parent,controlId, width, height, padding)
     };
     
     
+    
 };
+
+
 
 
